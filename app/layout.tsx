030fc653d5b30d817c71009a17d0da103960b2c9
mselbekk11 <mselbekk11@gmail.com>
2025-10-20 10:01:09 -0700

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { CartProvider } from 'components/cart/cart-context';
 import { Navbar } from 'components/layout/navbar';
 import { WelcomeToast } from 'components/welcome-toast';
-import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 import { getCart } from 'lib/shopify';
 import { baseUrl } from 'lib/utils';
 import { ReactNode } from 'react';
@@ -31,7 +31,7 @@ export default async function RootLayout({
   const cart = getCart();
 
   return (
-    <html lang='en' className={`${GeistSans.variable}`}>
+    <html lang='en' className={`${GeistMono.variable}`}>
       <body className='bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white'>
         <CartProvider cartPromise={cart}>
           <Navbar />
