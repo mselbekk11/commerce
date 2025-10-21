5e18564b7976295c64b43187e0612b3df7d77c1e
mselbekk11 <mselbekk11@gmail.com>
2025-10-21 10:11:40 -0700

    updates

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -11,11 +11,8 @@ export default async function Footer() {
   const copyrightName = COMPANY_NAME || SITE_NAME || '';
 
   return (
-    <footer className='text-sm text-neutral-500 dark:text-neutral-400'>
-      <div className='min-h-[200px] bg-black'>
-        Test
-      </div>
-      <div className='border-t border-neutral-700 py-6 text-sm bg-black'>
+    <footer className='text-sm text-neutral-500'>
+      <div className='py-12 md:pt-6 md:pb-30 text-sm bg-black'>
         <div className='mx-auto flex w-full flex-col items-center gap-1 px-4 md:flex-row md:gap-0 md:px-4 min-[1320px]:px-0'>
           <p>
             &copy; {copyrightDate} {copyrightName}
