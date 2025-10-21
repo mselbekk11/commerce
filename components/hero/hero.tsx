7fd81a2b00e835707476defd3b4552a809fa4410
mselbekk11 <mselbekk11@gmail.com>
2025-10-21 07:25:02 -0700

    updates

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <div className='w-full bg-black h-[600px]'>
-      <div>This is a test</div>
+      {/* <div>This is a test</div> */}
     </div>
   );
 }
