119e712befdc9e8a2e1400169d903e6c624cfd87
mselbekk11 <mselbekk11@gmail.com>
2025-10-22 09:54:12 -0700

    updates

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -2,10 +2,15 @@ import { AnimatedHalftoneBackgroundTwo } from './animated-halftone-background-tw
 
 export default function Hero() {
   return (
-    <div className='w-full h-[400px] bg-black'>
-      {/* <div>This is a test</div> */}
-      {/* <AnimatedHalftoneBackground /> */}
-      <AnimatedHalftoneBackgroundTwo />
+    <div className='relative w-full bg-black py-20 min-h-[600px]'>
+      <div className='absolute inset-0 '>
+        <AnimatedHalftoneBackgroundTwo />
+      </div>
+      <div className='relative z-10'>
+        {/* <div className='w-full max-w-2xl mx-auto flex flex-col gap-6 items-center justify-center'>
+          <p className='text-4xl font-bold text-white'>Title</p>
+        </div> */}
+      </div>
     </div>
   );
 }
--- components/section/section.tsx
+++ components/section/section.tsx
@@ -1,9 +1,15 @@
+import { AnimatedHalftoneBackgroundTwo } from '../hero/animated-halftone-background-two';
 import ClothingDetail from './clothing-detail';
 
 export default function Section() {
   return (
-    <div className='w-full bg-black py-20'>
-      <ClothingDetail />
+    <div className='relative w-full bg-black py-20'>
+      <div className='absolute inset-0'>
+        <AnimatedHalftoneBackgroundTwo />
+      </div>
+      <div className='relative z-10'>
+        <ClothingDetail />
+      </div>
     </div>
   );
 }
